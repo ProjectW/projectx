599bb4f873293d92082c2b8737a40bd8d9d4a7c8
Willy Xiao <wxiao@hcs.harvard.edu>
2014-07-04 17:46:59 -0700

    removed turbolinks, erb to haml, working on sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Projectx::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # sass
+  config.sass.preferred_syntax = :sass
+  config.sass.line_comments = false
+  config.sass.cache = false
 end
