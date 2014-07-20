d9a65140db9923b2e97fad35b46eda96761f377f
Willy Xiao <wxiao@hcs.harvard.edu>
2014-07-20 13:12:50 -0700

    resumes can be shown

--- app/controllers/student/resumes_controller.rb
+++ app/controllers/student/resumes_controller.rb
@@ -8,13 +8,23 @@ class Student::ResumesController < Student::StudentBaseController
   def create
     @resume = Resume.new(resume_params)
     @resume.student_account = @current_student
-    @current_student.current_resume = @resume
+    @current_student.current_resume = @resume # FIXME old resume should not have nil student
 
     if @resume.save && @current_student.save
       redirect_to :student_dashboard
     else
       throw "error!"
     end
+  end
+
+  def show
+    @resume = Resume.find(params.fetch(:id))
+    
+    if @resume.student_account_id != @current_student.id
+      throw "Error: unauthorized id access"
+    end
+
+    send_file(@resume.upload.path, :filename => 'resume.pdf', :type => 'application/pdf', :disposition => 'inline')
   end
 
   private
