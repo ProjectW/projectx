b9aba370388d6173aa3b766f615fe6b0d6b0495e
Willy Xiao <wxiao@hcs.harvard.edu>
2014-07-26 00:12:37 -0700

    new indentation

--- app/models/student_attribute_list.rb
+++ app/models/student_attribute_list.rb
@@ -3,9 +3,9 @@ class StudentAttributeList < ActiveRecord::Base
   has_many :interested_roles
 
   enumerate :technical_level do
-    value :name => 'none', :id => 0
-    value :name => 'low', :id => 100
-    value :name => 'medium', :id => 200
-    value :name => 'high', :id => 300
+    value :name => 'none',    :id => 0
+    value :name => 'low',     :id => 100
+    value :name => 'medium',  :id => 200
+    value :name => 'high',    :id => 300
   end
 end
