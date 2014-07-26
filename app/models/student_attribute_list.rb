29d704a311ab6bbb7e3cb4cd0a8ee6d38765e0f2
Willy Xiao <wxiao@hcs.harvard.edu>
2014-07-26 00:09:36 -0700

    add enums to interested_role and student_attribute_list class

--- app/models/student_attribute_list.rb
+++ app/models/student_attribute_list.rb
@@ -2,4 +2,10 @@ class StudentAttributeList < ActiveRecord::Base
   belongs_to :student_account
   has_many :interested_roles
 
+  enumerate :technical_level do
+    value :name => 'none', :id => 0
+    value :name => 'low', :id => 100
+    value :name => 'medium', :id => 200
+    value :name => 'high', :id => 300
+  end
 end
