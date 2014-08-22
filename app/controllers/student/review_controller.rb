e1e57bbd90d1fd42b5de84974830d69644369a7f
Lisa Wang <lisawang01@college.harvard.edu>
2014-08-22 15:27:46 -0400

    willy's comments and fixed the homepage sign up

--- app/controllers/student/review_controller.rb
+++ app/controllers/student/review_controller.rb
@@ -2,21 +2,21 @@ class Student::ReviewController < Student::StudentBaseController
   before_action :set_current_student
 
   def new
-    @reviews = Review.new
+    @review = Review.new
   end
 
   def create
-    @reviews = Review.new(review_params)
+    @review = Review.new(review_params)
 
-    if !@reviews.save
+    if !@review.save
       raise "Review did not save correctly"
     else
-      redirect_to :student_dashboard
+      redirect_to :sign_in
     end
   end
 
   def review_params
-    params.require(:reviews).permit(:email, :first_name, :last_name, :company, :position_title)
+    params.require(:review).permit(:email, :first_name, :last_name, :company, :position_title)
   end
 
 end
