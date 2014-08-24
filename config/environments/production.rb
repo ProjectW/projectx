1f843d2290b61c62cdc07a57e8d3c942c0a4612b
Lisa Wang <lisawang01@college.harvard.edu>
2014-08-24 17:11:31 -0400

    development and production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Projectx::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'http://pinpeg.io' } # devise
+  config.action_mailer.default_url_options = { host: 'pinpeg.io' } # devise
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
