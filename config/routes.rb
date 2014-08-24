b5a6717faf5857c7633c0f18f26d31bc13f54ef0
Willy Xiao <wxiao@hcs.harvard.edu>
2014-08-24 16:45:51 -0700

    accepting only reviews, everything else disabled. Revert this commit to start working

--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,36 @@
 Projectx::Application.routes.draw do
 
   namespace :student do
-    match 'dashboard/(:action)', 
-      :controller => :student_dashboard, 
-      :via => [:get], 
-      :defaults => { :action => 'show' }, 
-      :as => :dashboard
+  #   match 'dashboard/(:action)', 
+  #     :controller => :student_dashboard, 
+  #     :via => [:get], 
+  #     :defaults => { :action => 'show' }, 
+  #     :as => :dashboard
 
-    devise_for :account, :class_name => 'StudentAccount', :path => 'account', :path_names => {
-      :sign_in => 'login', 
-      :sign_out => 'logout',
-      :sign_up => 'register'
-    } 
+  #   devise_for :account, :class_name => 'StudentAccount', :path => 'account', :path_names => {
+  #     :sign_in => 'login', 
+  #     :sign_out => 'logout',
+  #     :sign_up => 'register'
+  #   } 
 
-    resources :resumes
+  #   resources :resumes
     resources :reviews
   end
 
-  # namespace :company do
-  #   match 'dashboard/(:action)',
-  #     :controller => :company_dashboard, 
-  #     :via => [:get],
-  #     :defaults => { :action => 'show' }
+  # # namespace :company do
+  # #   match 'dashboard/(:action)',
+  # #     :controller => :company_dashboard, 
+  # #     :via => [:get],
+  # #     :defaults => { :action => 'show' }
 
-  #   get 'payment/index'
-  # end
+  # #   get 'payment/index'
+  # # end
 
-  get 'home/index'
-  get '/about_us' => 'home#about_us'
+  # get 'home/index'
+  # get '/about_us' => 'home#about_us'
 
-root to: 'home#index'
+root to: 'student/reviews#index'
+# root to: 'home#index'
 
 
   # Example of regular route:
