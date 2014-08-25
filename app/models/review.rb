75b39526a5d5b36d9807622fe6808f138190dd25
Lisa Wang <lisawang01@college.harvard.edu>
2014-08-24 21:40:51 -0400

    change reviews table

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
 class Review < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :position_title, presence: true
+  validates :company, presence: true
+  validates :season, presence: true
+  validates :year, presence: true
+  validates :net_promoter, presence: true
+  validates :hours, presence: true
+  validates :number_interns, presence: true, numericality: {only_integer: true}
+  validates :salary, presence: true
+  validates :mentorship, presence: true
+  validates :projects, presence: true
+  validates :end, presence: true
 
 end
