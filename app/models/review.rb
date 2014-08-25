48ed9e979c52d2b8b1f42a4c648d580d1cfbac88
Willy Xiao <wxiao@hcs.harvard.edu>
2014-08-25 11:59:55 -0700

    verifying @college.harvard.edu email

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@college\.harvard\.edu\Z/i }
   validates :position_title, presence: true
   validates :company, presence: true
   validates :season, presence: true
