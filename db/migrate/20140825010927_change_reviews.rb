882c0eb7981f685ea0f738481bac37ba7c1f5e9c
Lisa Wang <lisawang01@college.harvard.edu>
2014-08-24 21:30:01 -0400

    change migration

--- db/migrate/20140825010927_change_reviews.rb
+++ db/migrate/20140825010927_change_reviews.rb
@@ -4,7 +4,6 @@ class ChangeReviews < ActiveRecord::Migration
       t.remove :weeks, :start_time, :end_time, :managers, :rant, :company_rank, :culture, :mgmt, :offer, :return, :recommend, :explain, :first_type, :first_difficulty, :first_questions, :second_type, :second_difficulty, :second_questions, :third_type, :third_difficulty, :third_questions, :fourth_type, :fourth_difficulty, :fourth_questions, :fifth_type, :fifth_difficulty, :fifth_questions
       t.string :hours,        :null => false
       t.string :mentorship,   :null => false
-      t.string :projects,     :null => false
       t.string :end,          :null => false
     end
   end
