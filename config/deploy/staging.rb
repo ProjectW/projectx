ee9f2bebe2ed96fbfa99d117425237b76d964ec3
Willy Xiao <wxiao@hcs.harvard.edu>
2014-08-31 00:25:16 -0400

    add fixme note

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 role :app, %w{root@104.131.27.192} # need to change to deploy@pinpeg.io
 
+# FIXME this is not precompiling assets or running rake
 namespace :deploy do
 
   desc 'Restart application'
