ff09085b06baf7c05c6fb80ca1b9ae3a537a9d13
Willy Xiao <wxiao@hcs.harvard.edu>
2014-08-30 23:08:44 -0400

    change file paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,13 +4,13 @@ working_directory "/var/www/pinpeg/current"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/pinpeg/pids/unicorn.pid"
+pid "/home/unicorn/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/pinpeg/log/unicorn.log"
-stdout_path "/var/www/pinpeg/log/unicorn.log"
+stderr_path "/home/unicorn/log/unicorn.log"
+stdout_path "/home/unicorn/log/unicorn.log"
 
 # Unicorn socket
 listen "127.0.0.1:8080"
