7c692d5d2f79efa85c3aaa4dd4ff0c66110f5737
Willy Xiao <wxiao@hcs.harvard.edu>
2014-08-30 23:55:29 -0400

    restart based on server now

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,16 @@
-# nothing set yet
 role :app, %w{root@104.131.27.192} # need to change to deploy@pinpeg.io
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      within '/var/wwww/pinpeg/current' do
+        execute 'kill `cat /home/unicorn/pids/unicorn.pid`'
+        execute 'bundle exec unicorn_rails -c config/unicorn.rb -D -E production'
+      end
+    end
+  end
+  after :publishing, :restart
+
+end
