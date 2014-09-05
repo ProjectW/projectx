10fbc2d2dd40c73943695dad7396381c4d5c40cf
Willy Xiao <wxiao@hcs.harvard.edu>
2014-09-04 23:27:09 -0400

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Projectx::Application.routes.draw do
 
   namespace :student do
     get '/dashboard' => 'student_dashboard#show'
-
     get '/company' => 'company_profile#show'
 
     # match 'dashboard/(:action)', 
@@ -26,7 +25,7 @@ Projectx::Application.routes.draw do
     # resources :companies
     resources :resumes do
       collection do
-        get 'current'
+        get :current
       end
     end
 
@@ -46,56 +45,6 @@ Projectx::Application.routes.draw do
   get 'home/index'
   get '/about_us' => 'home#about_us'
 
-# root to: 'student/reviews#index'
-root to: 'home#index'
-
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # root to: 'student/reviews#index'
+  root to: 'home#index'
 end
