b7b48ec510f73c8fa9a9804a3cc8d1ceaf76cdcb
karine-hsu <khsurox888@gmail.com>
2014-09-20 13:33:53 -0400

    company links

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,19 @@ schools = [
 ]
 
 companies = [
-  { id: 1, name: 'google', display_name: 'Google' },
-  { id: 2, name: 'palantir', display_name: 'Palantir' },
-  { id: 3, name: 'facebook', display_name: 'Facebook' },
-  { id: 4, name: 'harvard_management_company', display_name: 'Harvard Management Company' },
-  { id: 5, name: 'liveramp', display_name: 'LiveRamp' },
-  { id: 6, name: 'cueball', display_name: 'Cue Ball' }
+  { id: 1, name: 'google', display_name: 'Google', url: 'http://www.google.com/about/careers/'},
+  { id: 2, name: 'palantir', display_name: 'Palantir', url: 'https://www.palantir.com/college/'},
+  { id: 3, name: 'facebook', display_name: 'Facebook', url: 'https://www.facebook.com/careers/university' },
+  { id: 4, name: 'harvard_management_company', display_name: 'Harvard Management Company', url:'https://harvardcareers.silkroad.com/' },
+  { id: 5, name: 'liveramp', display_name: 'LiveRamp', url: 'http://liveramp.com/careers/' },
+  { id: 6, name: 'cueball', display_name: 'Cue Ball', url: 'http://www.cueball.com/' },
+  { id: 7, name: 'microsoft', display_name: 'Microsoft', url: 'http://careers.microsoft.com/careers/en/us/collegehome.aspx' },
+  { id: 8, name: 'hubspot', display_name: 'HubSpot', url: 'http://www.hubspot.com/jobs' },
+  { id: 9, name: 'quora', display_name: 'Quora', url: 'http://www.quora.com/careers'},
+  { id: 10, name: 'linkedin', display_name: 'LinkedIn', url: 'http://www.linkedin.com/company/linkedin/careers' },
+  { id: 11, name: 'dropbox', display_name: 'Dropbox', url: 'https://www.dropbox.com/jobs' },
+  { id: 12, name: 'vivint', display_name: 'vivint', url: 'https://vivintjobs.silkroad.com/' }
+
 ]
 
 def seed_database(model, records)
