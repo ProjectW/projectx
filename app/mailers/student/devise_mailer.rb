751ab30b82c6ec91aeaaf8b1bc80ae460c9ac942
Willy Xiao <willyxiao@gmail.com>
2014-10-01 07:16:48 -0400

    hello@pinpeg.io rather than no-reply

--- app/mailers/student/devise_mailer.rb
+++ app/mailers/student/devise_mailer.rb
@@ -1,7 +1,7 @@
 class Student::DeviseMailer < Devise::Mailer
   self.delivery_method = :sendmail
 
-  default from: "no-reply@pinpeg.io"
+  default from: "hello@pinpeg.io"
   default reply_to: "no-reply@pinpeg.io"
 
 end
