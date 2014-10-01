8aa51f9f0f1a54acbb1d157acb43936a7cb1f445
karine-hsu <khsurox888@gmail.com>
2014-10-01 15:27:28 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,6 +69,7 @@ companies = [
   { id: 56, name: 'yelp', display_name: 'Yelp', url: 'http://www.yelp.com/careers'},
   { id: 57, name: 'riotgames', display_name: 'Riot Games', url: 'http://www.riotgames.com/NAinternships'},
   { id: 58, name: 'nutraclick', display_name: 'Nutraclick', url: 'http://www.nutraclick.com/join/internship-program'},
+  { id: 59, name: 'earnest', display_name: 'Earnest', url: 'https://www.meetearnest.com/meet-earnest'}
 
 
 ]
