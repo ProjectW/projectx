7a44f47eb2ef9f0fd37c2fc75a9cbcb4fd9e38a0
Willy Xiao <willyxiao@gmail.com>
2014-09-30 22:29:23 -0400

    change routes and footer for about

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@
 Projectx::Application.routes.draw do
 
   get 'home/index'
-  # get '/about_us' => 'home#about_us'
-  get '/hmmmm' => 'home#about'
-  get '/hmmm' => 'home#about'
-  get '/hmm' => 'home#about'
-  get '/hm' => 'home#about'
+  get '/about' => 'home#about'
   get '/privacy' => 'home#privacy'
   get '/contact' => 'home#get_contact'
   post '/contact' => 'home#post_contact'
