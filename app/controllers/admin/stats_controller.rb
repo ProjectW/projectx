02afe55bb3c46c4180387a5cd4b96f7f270f5401
Willy Xiao <willyxiao@gmail.com>
2014-10-03 23:51:22 -0400

    review count

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -5,7 +5,6 @@ class Admin::StatsController < Admin::AdminBaseController
   def index
     @confirmed_accounts = StudentAccount.count
     @unconfirmed_accounts = StudentAccount.where(:confirmed_at => nil).count
-    @reviews = Review.count
     @uniq_resumes = Resume.all.map(&:student_account_id).uniq.length
 
     @reviews = Review.order(:created_at => :desc).limit(REVIEWS_LIMIT)
