9efef9633aeeae5cb0e83741cc149217e224365d
Willy Xiao <willyxiao@gmail.com>
2014-10-04 00:00:55 -0400

    more stats

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -5,6 +5,8 @@ class Admin::StatsController < Admin::AdminBaseController
   def index
     @confirmed_accounts = StudentAccount.count
     @unconfirmed_accounts = StudentAccount.where(:confirmed_at => nil).count
+    @one_page_view = CompanyProfileView.select(:student_account_id).distinct.count
+    @total_page_views = CompanyProfileView.where.not(:student_account_id => StudentAccount.get_admins.map(&:id)).count
     @uniq_resumes = Resume.all.map(&:student_account_id).uniq.length
 
     @reviews = Review.order(:created_at => :desc).limit(REVIEWS_LIMIT)
