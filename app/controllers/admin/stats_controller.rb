840340601093e5d3a46c100d001e6ff79fcf9202
Willy Xiao <willyxiao@gmail.com>
2014-10-10 17:11:15 -0400

    new stats

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -6,7 +6,8 @@ class Admin::StatsController < Admin::AdminBaseController
     @confirmed_accounts = StudentAccount.where.not(:confirmed_at => nil).count
     @unconfirmed_accounts = StudentAccount.where(:confirmed_at => nil).count
     @one_page_view = CompanyProfileView.select(:student_account_id).distinct.count
-    @two_page_view = CompanyProfileView.select(:student_account_id).group(:student_account_id).count.keep_if{ |k, v| v > 2}.length
+    @three_page_view = CompanyProfileView.select(:student_account_id).group(:student_account_id).count.keep_if{ |k, v| v > 2}.length
+    @ten_page_view = CompanyProfileView.select(:student_account_id).group(:student_account_id).count.keep_if{ |k, v| v > 9}.length
     @total_page_views = CompanyProfileView.where.not(:student_account_id => StudentAccount.get_admins.map(&:id)).count
     @uniq_resumes = Resume.all.map(&:student_account_id).uniq.length
 
