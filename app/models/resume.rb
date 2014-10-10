a39541a4afbc366189f454910602895ede14241c
Willy Xiao <willyxiao@gmail.com>
2014-10-10 18:07:59 -0400

    new zip function

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,7 +1,7 @@
 class Resume < ActiveRecord::Base
   require 'zip'
 
-  ZIP_BASE_PATH = Rails.env.production? ? '/var/pinpeg' : File.join(Rails.root, 'tmp')
+  BASE_PATH = Rails.env.production? ? '/var/pinpeg' : File.join(Rails.root, 'tmp')
 
   belongs_to :student_account
 
@@ -9,12 +9,11 @@ class Resume < ActiveRecord::Base
 
   # https://github.com/thoughtbot/paperclip
   PATH_TO_RESUME = '/resumes/account:student_account_id/:style:updated_at.:extension'
-  BASE_PATH = Rails.env.production? ? '/var/pinpeg' : ':rails_root/tmp'
 
   has_attached_file :upload, {
     :preserve_files => 'true',
     :url => PATH_TO_RESUME,
-    :path => BASE_PATH + PATH_TO_RESUME
+    :path => File.join(BASE_PATH, PATH_TO_RESUME)
   }
   validates_attachment_content_type :upload, :content_type => ['application/pdf', 'application/x-pdf']
 
@@ -22,16 +21,15 @@ class Resume < ActiveRecord::Base
     self.student_account.current_resume == self
   end
 
+  def self.current_resumes
+    Resume.find(StudentAccount.select(:current_resume_id).map(&:current_resume_id))
+  end
+
   def self.make_zip(name = nil, resumes = nil)
     name = "resumes" if !name
-    zip_path = File.join(ZIP_BASE_PATH, "zip", name + Time.now.to_i.to_s) + ".zip"
+    zip_path = File.join(BASE_PATH, "zip", name + Time.now.to_i.to_s) + ".zip"
 
-    if !resumes
-      current_resume_ids = StudentAccount.select(:current_resume_id).all.map(&:current_resume_id)
-      resumes = Resume.find(current_resume_ids)
-    end
-
-    files = resumes.map{ |resume| resume.upload.path }
+    files = resumes ? resumes.map{ |resume| resume.upload.path } : [File.join(BASE_PATH, "resumes")]
 
     Zip::File.open(zip_path, Zip::File::CREATE) do |zipfile|
       files.each do |file_to_be_zipped|
@@ -39,7 +37,7 @@ class Resume < ActiveRecord::Base
         if File.directory?(file_to_be_zipped)
           directory = file_to_be_zipped
           puts "zipper: archiving directory: #{directory}"
-          File.dirname(directory)
+          directory_chosen_pathname = File.dirname(directory)
           directory_pathname = Pathname.new(directory_chosen_pathname)
           Dir[File.join(directory, '**', '**')].each do |file|
             file_pathname = Pathname.new(file)
