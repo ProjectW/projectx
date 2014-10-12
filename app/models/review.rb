ed66cdc0085391eb1466f518e85edcb86e0ea63a
Willy Xiao <willyxiao@gmail.com>
2014-10-12 11:41:52 -0400

    delete review comments

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,21 +4,10 @@ class Review < ActiveRecord::Base
   belongs_to :student_account
   belongs_to :company
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, presence: true
-  # validates :email, format: { with: /\A([^@\s]+)@college\.harvard\.edu\Z/i }
   validates :position_title, presence: true
   validates :company, presence: true
   validates :season, presence: true
   validates :year, presence: true
-  # validates :net_promoter, presence: true
-  # validates :hours, presence: true
-  # validates :number_interns, presence: true, numericality: { only_integer: true }
-  # validates :mentorship, presence: true
-  # validates :projects, presence: true
-  # validates :story, presence: true
-  # validates :culture, presence: true
 
   after_create :notify_admin
 
