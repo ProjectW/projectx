a2c77b3e0297b2c41a488928761d1b8fb862c307
Willy Xiao <willyxiao@gmail.com>
2014-10-12 10:51:24 -0400

    Revert "add index to watches"
    This reverts commit eea0b3ae17a0ed9c7432f34b5b980b579061c84e.

--- db/migrate/20141012144703_add_index_to_watches.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToWatches < ActiveRecord::Migration
-  def self.up
-    add_index "company_profile_watches", ["company_id", "student_account_id"], :name => "index_on_company_student_account_id"
-    add_index "company_profile_watches", ["watching"], :name => "index_on_watching"
-  end
-end
